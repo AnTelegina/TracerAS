facb024c26b989659c97236f0b46db1e3b55ff34
Anastasia <anastasia_telegina@mail.ru>
2020-06-07 13:20:14 +0500

    third commit

--- trace.py
+++ trace.py
@@ -1,4 +1,9 @@
 import sys
 import os
+from subprocess import check_output
 
-print(os.system('tracert' + ' '+ sys.argv[1]))
+#result = os.system('tracert -d' + ' '+ sys.argv[1])
+
+result = check_output('tracert -4 -d ' + sys.argv[1], shell=True).decode('cp866')
+
+print(result)
