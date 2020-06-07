859e7f23ca472fbf7a98ce81c18d5d0d31fc0af3
Anastasia <anastasia_telegina@mail.ru>
2020-06-07 13:28:30 +0500

    fourth commit

--- trace.py
+++ trace.py
@@ -1,9 +1,16 @@
 import sys
 import os
+from re import findall
 from subprocess import check_output
 
 #result = os.system('tracert -d' + ' '+ sys.argv[1])
 
 result = check_output('tracert -4 -d ' + sys.argv[1], shell=True).decode('cp866')
 
-print(result)
+#print(result)
+
+list_of_ip_adresses = findall('\d+.\d+.\d+.\d+', result)
+list_of_ip_adresses.pop(0)
+
+print(list_of_ip_adresses)
+
